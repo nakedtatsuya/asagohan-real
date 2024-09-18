821ca61c2380298300f7c1f24df36a68b54db76d
marina <marina0724312@gmail.com>
2024-09-18 16:18:49 +0900

    結構いい感じです

--- app/user/[accountID]/page.tsx
+++ app/user/[accountID]/page.tsx
@@ -1,20 +1,47 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { Avatar } from "@mui/material";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Link href={"/"}><div>←</div></Link><h1 className={styles.h1}>ユーザプロフィール!
 
-        <div>アカウント名<Image src="/user_image.png" alt={"userIcon"} height={100} width={100} /></div>
-        <div>べすと朝ごはん！
-          <Image src="/breakfast_image.png" alt={"AsagohanPicture"} height={100} width={150} />
+      <Link href={"/"}>←
+      </Link>
+
+      <h1 className={styles.h1}>ユーザプロフィール</h1>
+
+      <div>
+      </div>
+      <Avatar src="/user_image.png" alt={"userIcon"} sx={{ width: '200px', height: '200px' }}></Avatar>
+      <div className={styles.account_name}>アカウント名
+      </div>
+
+      <div className={styles.profile1}>
+        <div className={styles.iconTextContainer}><Image
+          src="/icon512_maskable.png"
+          alt={"Icon"}
+          height={50}
+          width={50}
+        />
+          <div>べすと朝ごはん!</div>
         </div>
-        <div>今週の朝ごはん <Image src="/breakfast_image.png" alt={"WeeklyAsagohan"} height={100} width={150} />
-          <div>○月○日</div></div>
+        <Image src="/breakfast_image.png" alt={"AsagohanPicture"} height={100} width={150}
+        />
+      </div>
 
-      </h1>
-    </main>
+      <div className={styles.profile1}>
+        <div className={styles.iconTextContainer}><Image
+          src="/icon512_maskable.png"
+          alt={"Icon"}
+          height={50}
+          width={50} />
+          <div>今週の朝ごはん </div>
+        </div>
+        <Image src="/breakfast_image.png" alt={"WeeklyAsagohan"} height={100} width={150}
+        />○月○日
+      </div>
+    </main >
   );
 }
