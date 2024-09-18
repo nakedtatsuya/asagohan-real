2d32f372bb0ae3929a116d3110f3f7a81cc25ec9
marina <marina0724312@gmail.com>
2024-09-18 11:48:23 +0900

    画像表示されるようになりました！

--- app/user/[accountID]/page.tsx
+++ app/user/[accountID]/page.tsx
@@ -4,20 +4,17 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Link href={"/"}><div>←</div></Link><h1 className={styles.h1}>ユーザプロフィール!
+    <main className={styles.main}>
+      <Link href={"/"}><div>←</div></Link><h1 className={styles.h1}>ユーザプロフィール!
 
-          <div>アカウント名<img src="user_image.png"></img></div>
-          <div>べすと朝ごはん！
-            <img src="breakfast_image.png"></img>
-          </div>
-          <div>今週の朝ごはん <img src="breakfast_image.png"></img>
-            <div>○月○日</div></div>
+        <div>アカウント名<Image src="/user_image.png" alt={"userIcon"} height={100} width={100} /></div>
+        <div>べすと朝ごはん！
+          <Image src="/breakfast_image.png" alt={"AsagohanPicture"} height={100} width={150} />
+        </div>
+        <div>今週の朝ごはん <Image src="/breakfast_image.png" alt={"WeeklyAsagohan"} height={100} width={150} />
+          <div>○月○日</div></div>
 
-        </h1>
-      </main>
-
-    </div>
+      </h1>
+    </main>
   );
 }
