fdf0bd91e2253892d1f15b90edb93afb0744fb6a
yui-119 <poppop0206@icloud.com>
2024-09-20 01:04:57 +0900

    みんなの投稿表示できたよ

--- app/page.tsx
+++ app/page.tsx
@@ -13,55 +13,19 @@ import Box from '@mui/material/Box';
 import SendIcon from '@mui/icons-material/Send';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Asagohan from "./types/Asagohan";
 
-const testAsagohan =
-{
-  id: "1",
-  createdAt: "8時30分",
-  title: "test朝ごはん",
-  imagePath: "https://prkmeuqkrooltclacpzl.supabase.co/storage/v1/object/public/asagohans/2.png",
-  likes: 10,
-  isLiked: true,
-  ranking: 1,
-  comments: [
-    {
-      content: "おいしそう!",
-      createdAt: "8時31分",
-      user: {
-        name: "testユーザー",
-        accountID: "test-user",
-        userIconPath:
-          "https://prkmeuqkrooltclacpzl.supabase.co/storage/v1/object/public/user_icons/79441e4f-39bc-4d2e-978b-68d5907000c2.png",
-      },
-    },
-    {
-      content: "早起きえらい",
-      createdAt: "10時31分",
-      user: {
-        name: "testユーザー",
-        accountID: "test-user",
-        userIconPath:
-          "https://prkmeuqkrooltclacpzl.supabase.co/storage/v1/object/public/user_icons/79441e4f-39bc-4d2e-978b-68d5907000c2.png",
-      },
-    },
-  ],
-  user: {
-    name: "test-yuka",
-    accountID: "test-user",
-    userIconPath:
-      "https://prkmeuqkrooltclacpzl.supabase.co/storage/v1/object/public/user_icons/79441e4f-39bc-4d2e-978b-68d5907000c2.png",
-  },
-};
 export default function Home() {
   const [userID] = useState("b2113406-aaaf-43bc-a32c-a5cc003506d7");
-  const { asagohans, todayAsagohansFetching } = useTodayAsagohans(userID);
+  const { asagohans, todayAsagohansFetching, onClickLike } = useTodayAsagohans(userID);
   console.log(asagohans, todayAsagohansFetching);
 
   // const [flag, setFlag] = useState(false); // 初期値はfalse
   // console.log(flag);
 
-  const handleClick = () => {
+  const handleClick = (asagohan: Asagohan) => {
     console.log("クリックされました");
+    onClickLike(asagohan)
   };
 
   const [open, setOpen] = React.useState(false);
@@ -122,128 +86,138 @@ export default function Home() {
         </div>
       </Header>
       <main className={styles.main}>
-        <Drawer
-          anchor="bottom"
-          open={open}
-          onClose={toggleDrawer(false)}
-        >
-          <div className={styles.drawer}
-            role="presentation">
-            <div className={styles.usercomment}>
-              <div className={styles.useravatar}>
-                <Avatar
-                  alt="投稿者イラスト"
-                  src={testAsagohan.user.userIconPath}
+        {asagohans?.map((asagohan, index) => {
+          return (
+            <div key={index} className={styles.userpush}>
+              <Drawer
+                anchor="bottom"
+                open={open}
+                onClose={toggleDrawer(false)}
+              >
+                <div className={styles.drawer}
+                  role="presentation">
+                  {asagohan.comments.map(comment => {
+                    return (
+                      <div className={styles.usercomment}>
+                        <div className={styles.useravatar}>
+                          <Avatar
+                            alt="コメント者イラスト"
+                            src={asagohan.user.userIconPath}
+                          />
+                        </div>
+                        <div className={styles.timecomment}>
+                          <p style={{ marginTop: "0", marginBottom: "0" }}>
+                            <span style={{ color: '#402011' }}>
+                              {comment.user.name}
+                            </span>
+                            <span style={{ color: '#605b58', paddingLeft: "10px" }}>
+                              {comment.createdAt}
+                            </span>
+                          </p>
+                          <p style={{ color: '#402011', marginTop: "0", marginBottom: "0" }}>
+                            {/* {asagohan.comments[0].content} */}
+                          </p>
+                        </div>
+                      </div>
+                    )
+                  })}
+                  <div className={styles.commentpush}>
+                    <Box
+                      component="form"
+                      sx={{
+                        '& .MuiTextField-root': { m: 1, width: '25ch' }
+                      }}
+                      noValidate
+                      autoComplete="off"
+                    />
+                    <TextField
+                      label="コメントを入力..."
+                      id="outlined-size-small"
+                      fullWidth
+                      sx={{
+                        // 入力値にフォントを適用
+                        "& .MuiOutlinedInput-root": { fontFamily: "var(--font)" },
+                        "& input": {
+                          color: "var(--primary)",
+                        },
+                        "& label": {
+                          fontFamily: "var(--font)",
+                        },
+                      }}
+                    />
+                    <Stack direction="row" spacing={2}>
+                      <Button variant="contained" endIcon={<SendIcon />}>
+                        Send
+                      </Button>
+                    </Stack>
+                  </div>
+                </div>
+              </Drawer>
+              <div className={styles.acount}>
+                <div className={styles.third}>
+                  <Avatar
+                    alt="投稿者イラスト"
+                    src={asagohan.user.userIconPath}
+                  />
+                  <p>{asagohan.user.name}</p>
+                </div>
+                <p className={styles.time}>
+                  {asagohan.createdAt}
+                </p>
+              </div>
+              <div className={styles.container}>
+                <Image className={styles.post}
+                  src={asagohan.imagePath}
+                  alt="朝ごはん投稿画像"
+                  width={319}
+                  height={229}
                 />
               </div>
-              <div className={styles.timecomment}>
-                <p style={{ marginTop: "0", marginBottom: "0" }}>
-                  <span style={{ color: '#402011' }}>
-                    {testAsagohan.user.name}
-                  </span>
-                  <span style={{ color: '#605b58', paddingLeft: "10px" }}>
-                    {testAsagohan.createdAt}
-                  </span>
-                </p>
-                <p style={{ color: '#402011', marginTop: "0", marginBottom: "0" }}>
-                  {testAsagohan.comments[0].content}
+              <div className={styles.forth}>
+                <div className={styles.button}>
+                  <div className={styles.good}>
+                    <Image onClick={() => handleClick(asagohan)} className={styles.goodbutton}
+                      src={asagohan.isLiked ? "いいね後のボタン.svg" : "いいね前のボタン.svg"}
+                      alt="いいね前のボタン画像"
+                      width={25}
+                      height={25}
+                    />
+                    <p className={styles.goodcount}>
+                      {asagohan.likes}
+                    </p>
+                  </div>
+                  <div onClick={toggleDrawer(true)} style={{ cursor: 'pointer' }}>
+                    <div className={styles.comment}>
+                      <Image className={styles.commentbutton}
+                        src="コメントボタン.svg"
+                        alt="コメントボタン画像"
+                        width={25}
+                        height={25}
+                      />
+                      <p className={styles.commentcount}>
+                        {asagohan.comments.length}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <p className={styles.title}>
+                  {asagohan.title}
                 </p>
               </div>
-            </div>
-            <div className={styles.commentpush}>
-              <Box
-                component="form"
-                sx={{
-                  '& .MuiTextField-root': { m: 1, width: '25ch' }
-                }}
-                noValidate
-                autoComplete="off"
-              />
-              <TextField
-                label="コメントを入力..."
-                id="outlined-size-small"
-                fullWidth
-                sx={{
-                  // 入力値にフォントを適用
-                  "& .MuiOutlinedInput-root": { fontFamily: "var(--font)" },
-                  "& input": {
-                    color: "var(--primary)",
-                  },
-                  "& label": {
-                    fontFamily: "var(--font)",
-                  },
-                }}
-              />
-              <Stack direction="row" spacing={2}>
-                <Button variant="contained" endIcon={<SendIcon />}>
-                  Send
-                </Button>
-              </Stack>
-            </div>
-          </div>
-        </Drawer>
-        <div className={styles.acount}>
-          <div className={styles.third}>
-            <Avatar
-              alt="投稿者イラスト"
-              src={testAsagohan.user.userIconPath}
-            />
-            <p>{testAsagohan.user.name}</p>
-          </div>
-          <p className={styles.time}>
-            {testAsagohan.createdAt}
-          </p>
-        </div>
-        <div className={styles.container}>
-          <Image className={styles.post}
-            src={testAsagohan.imagePath}
-            alt="朝ごはん投稿画像"
-            width={319}
-            height={229}
-          />
-        </div>
-        <div className={styles.forth}>
-          <div className={styles.button}>
-            <div className={styles.good}>
-              <Image onClick={handleClick} className={styles.goodbutton}
-                src={testAsagohan.isLiked ? "いいね前のボタン.svg" : "いいね後のボタン.svg"}
-                alt="いいね前のボタン画像"
-                width={25}
-                height={25}
-              />
-              <p className={styles.goodcount}>
-                {testAsagohan.likes}
-              </p>
-            </div>
-            <div onClick={toggleDrawer(true)} style={{ cursor: 'pointer' }}>
-              <div className={styles.comment}>
-                <Image className={styles.commentbutton}
-                  src="コメントボタン.svg"
-                  alt="コメントボタン画像"
-                  width={25}
-                  height={25}
+              <div className={styles.rankingstar}>
+                <Image className={styles.star}
+                  src="ランキング星画像.svg"
+                  alt="ランキング星画像"
+                  width={70}
+                  height={70}
                 />
-                <p className={styles.commentcount}>
-                  {testAsagohan.comments.length}
+                <p className={styles.rankingcount}>
+                  {asagohan.ranking}
                 </p>
               </div>
             </div>
-          </div>
-          <p className={styles.title}>
-            {testAsagohan.title}
-          </p>
-        </div>
-        <div className={styles.rankingstar}>
-          <Image className={styles.star}
-            src="ランキング星画像.svg"
-            alt="ランキング星画像"
-            width={70}
-            height={70}
-          />
-          <p className={styles.rankingcount}>
-            {testAsagohan.ranking}
-          </p>
-        </div>
+          )
+        })}
       </main>
     </div >
   );
