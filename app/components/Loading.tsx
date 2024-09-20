c92c30460ff966fc5ac3686ce98635ab82d94d47
marina <marina0724312@gmail.com>
2024-09-20 11:34:55 +0900

    loading-components

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,5 +1,5 @@
 "use client";
-import styles from "./page.module.css";
+import styles from "./loading.module.css";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,7 +7,6 @@ export default function Home() {
         <div className={styles.page}>
             <Image className={styles.bread} src="/bread.png" alt="bread" width={200} height={200} />
             <div>loading......</div>
-            </div>
-
+        </div>
     );
 }
